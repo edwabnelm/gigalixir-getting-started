0eb45aa70228a1b3e66a0c7aadf05d33d197ee66
edwabnelm <104506790+edwabnelm@users.noreply.github.com>
2022-11-15 09:12:12 +0000

    update

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :gigalixir_getting_started, GigalixirGettingStarted.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :gigalixir_getting_started, GigalixirGettingStartedWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4001],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
